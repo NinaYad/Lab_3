0f7219249aa3e7a84c9cc63c9222fce882eb51dc
NinaYad <113794163+NinaYad@users.noreply.github.com>
2022-12-12 12:59:13 +0300

    Update Task 2.py
    Actually done

--- Task 2.py
+++ Task 2.py
@@ -1,46 +1,49 @@
-# O(nlogn)
+import itertools
 
-def Qsort(mass):
-    if len(mass) <= 1:
-        return mass
-    else:
-        pillar = mass[0]
-        SmallerThanPillar = []
-        EqualPillar = []
-        BiggerThanPillar = []
-        for el in mass:
-            if el < pillar:
-                SmallerThanPillar.append(el)
-            elif el > pillar:
-                BiggerThanPillar.append(el)
-            else:
-                EqualPillar.append(el)
-        return Qsort(SmallerThanPillar) + EqualPillar + Qsort(BiggerThanPillar)
-
-
-# Придумать и реализовать алгоритмы, имеющие сложность O(nlogn), O(n!), O(3log(n))
+n = int(input())
 
 # O(n**3)
-def On3(n):
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                flag = True
-    return ''
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            flag = True
+
 
 # O(2**n)
 
 def DoubleRecursive(n):
     if n > 0:
-        return DoubleRecursive(n - 1), DoubleRecursive(n - 1)
+        DoubleRecursive(n - 1)
+        DoubleRecursive(n - 1)
     return ''
 
 
 # O(3n)
 
-def O3n(n):
-    if n > 0:
-        print('')
-        print('')
-        print('')
-    return ''
+for i in range(n):
+    print('/')
+for i in range(n):
+    print('/')
+for i in range(n):
+    print('/')
+
+# O(3logn)
+
+a = list(i for i in range(n))
+a.remove(0)
+a.remove(2)
+a.remove(1)
+print(a)
+
+# O(nlogn)
+
+a = list(i for i in range(n))
+a.sort()
+print(a)
+
+# O(n!)
+
+perm_set = itertools.permutations(a)
+for i in perm_set:
+    print(i)
